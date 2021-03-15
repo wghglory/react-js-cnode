a24dd2e287a72477d35f2fe412c6ca0c2a7a1a7a
Guanghui Wang <guanghui-wang@foxmail.com>
2021-03-15 23:01:43 +0800

    feat: add jsconfig.json

--- src/pages/Topic/index.js
+++ src/pages/Topic/index.js
@@ -2,7 +2,9 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Card } from 'antd';
-import { useGetTopic } from '../../store/action/topic';
+
+// import { useGetTopic } from '../../store/action/topic';  // since jsconfig.json is configured, we can use below absolute path
+import { useGetTopic } from 'store/action/topic';
 
 function TopicPage() {
   const { loading, data } = useSelector((state) => state.topic);
